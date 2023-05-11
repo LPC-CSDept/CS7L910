de5abc23be83374efd6b2e296d9f95a41d54c4f2
Kyuwoong Lee <kwooler@gmail.com>
2023-05-10 23:10:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+    heading = ['id', 'name', 'address']
+    valueset = [[10, 'Kim', '123 Main'],
+                [20, 'Bill', '345 Grand'],
+                [30, 'Mary', '123 Blvd']
+                ]
+
+    students = []
+    #########################################
+    # Code your program here
+    #########################################
+
+    #########################################
+    for v in students:
+        print(v)
+    return students
+    #########################################
+    # students
+    # [{'id': 10, ‘name': 'Kim', 'Address': ‘123 Main’},
+    # {'id': 20, 'nameI': 'Bill', ‘Address': '345 Grand'},
+    # {'id': 30, 'name': 'Mary', 'Address': '123 Blvd'}]
+
+
+if __name__ == '__main__':
+    main()
---
+++ main_test.py
@@ -0,0 +1,33 @@
+import main
+import io
+import sys
+import re
+import math
+import types
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
+    # sys.stdin = io.StringIO(datastr)
+
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    students = main.main()
+    assert len(students) == 3
+    assert students[0] == {'id': 10, 'name': 'Kim', 'address': '123 Main'}
+    assert students[1] == {'id': 20, 'name': 'Bill', 'address': '345 Grand'}
+    assert students[2] == {'id': 30, 'name': 'Mary', 'address': '123 Blvd'}
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
